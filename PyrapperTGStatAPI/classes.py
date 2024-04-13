c0a370a7a9f77256112472768b762d61c622c2c3
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-13 21:52:28 +0300

    [ADD] Forward add args

--- PyrapperTGStatAPI/classes.py
+++ PyrapperTGStatAPI/classes.py
@@ -105,12 +105,13 @@ class Mention:
 
 
 class Forward:
-    def __inint__(self, forwardId, sourcePostId, postId, postLink, postLink, channelId):
+    def __inint__(self, forwardId, sourcePostId, postId, postLink, postLink, channelId, peerType=None):
         self.forwardId = forwardId
         self.sourcePostId = sourcePostId
         self.postId = postId
         self.postLink = postLink
         self.channelId = channelId
+        self.peerType = peerType
 
 
 class Post:
