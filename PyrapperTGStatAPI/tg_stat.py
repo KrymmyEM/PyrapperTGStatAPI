6888805be47af32b492430e40b1184b7c0e26062
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-13 15:31:05 +0300

    [UP] TGStatSync

--- PyrapperTGStatAPI/tg_stat.py
+++ PyrapperTGStatAPI/tg_stat.py
@@ -1,6 +1,14 @@
-import requests
+from requests import Request, Session
 import aiohttp
 
 from .enums import *
 from .classes import *
 from .exceptions import * 
+
+
+class TGStatSync(Session):
+    
+    def __init__(self, token):
+        super().__init__()
+        self.token = token
+        
