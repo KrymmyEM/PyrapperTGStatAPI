7f0ba2ca3a1fd56a4c7ecdf91f8c33d4907111d9
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-13 20:17:11 +0300

    [EX] TGStatSyncTest [test_get_databases()]

--- tests/test_TGStatSync.py
+++ tests/test_TGStatSync.py
@@ -15,9 +15,17 @@ class TGStatSyncTest(unittest.TestCase):
         
     def test_get_databases(self):
         tgs = tg_stat.TGStatSync(environ.get("TOKEN"))
-        self.assertIsInstance(tgs.api(enums.RequestsCategory.DATABASE, enums.DatabaseRequests.CATEGORIES), classes.DatabaseEntity, "Database not return DatabaseEntity in categories")
-        self.assertIsInstance(tgs.api(enums.RequestsCategory.DATABASE, enums.DatabaseRequests.COUNTRIES), classes.DatabaseEntity, "Database not return DatabaseEntity in countries")
-        self.assertIsInstance(tgs.api(enums.RequestsCategory.DATABASE, enums.DatabaseRequests.LANGUAGES), classes.DatabaseEntity, "Database not return DatabaseEntity in langiages")
+        categories = tgs.api(enums.RequestsCategory.DATABASE, enums.DatabaseRequests.CATEGORIES)
+        countries = tgs.api(enums.RequestsCategory.DATABASE, enums.DatabaseRequests.COUNTRIES)
+        languages = tgs.api(enums.RequestsCategory.DATABASE, enums.DatabaseRequests.LANGUAGES)
+        self.assertIsInstance(categories, classes.DatabaseEntity, "Database not return DatabaseEntity in categories")
+        self.assertIsInstance(countries, classes.DatabaseEntity, "Database not return DatabaseEntity in countries")
+        self.assertIsInstance(languages, classes.DatabaseEntity, "Database not return DatabaseEntity in languages")
+
+        self.assertEqual(categories.database_type, enums.DatabaseTypes.CATEGORIES, "Not correct db type in category")
+        self.assertEqual(categories.database_type, enums.DatabaseTypes.COUNTRIES, "Not correct db type in countries")
+        self.assertEqual(categories.database_type, enums.DatabaseTypes.LANGUAGES, "Not correct db type in languages")
+        
     
     def test_get_channel_info(self):
         tgs = tg_stat.TGStatSync(environ.get("TOKEN"))
