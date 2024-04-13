067624272d7a56a76ef1b774714416cf3a322c7a
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-13 13:44:28 +0300

    [INIT] Classes Branch

---
+++ PyrapperTGStatAPI/classes.py
@@ -0,0 +1,29 @@
+
+class Channel:
+    def __init__(self,):
+        pass
+
+
+class User:
+    def __init__(self, ):
+        pass
+    
+
+class Post:
+    def __init__(self, ):
+        pass
+
+
+class Story:
+    def __init__(self, ):
+        pass
+
+
+class Media:
+    def __init__(self, ):
+        pass
+
+
+class CallbackNotify:
+    def __init__(self, ):
+        passs
