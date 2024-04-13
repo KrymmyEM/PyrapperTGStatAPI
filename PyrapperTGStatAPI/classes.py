a7052ff34958d2cc4cdbea81ddf152619769dae5
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-13 13:48:18 +0300

    [UP] Channel add init args

--- PyrapperTGStatAPI/classes.py
+++ PyrapperTGStatAPI/classes.py
@@ -1,13 +1,16 @@
 
 class Channel:
-    def __init__(self,):
+    def __init__(
+            self, ts_id, tg_id, link, peer_type, username, active_usernames,
+            title, about, image100, image640, participants_count, tgstat_restrictions
+        ):
         pass
 
 
 class User:
     def __init__(self, ):
         pass
-    
+
 
 class Post:
     def __init__(self, ):
