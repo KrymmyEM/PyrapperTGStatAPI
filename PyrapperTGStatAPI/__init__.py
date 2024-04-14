26fe750d2c64057aab8e785c5d86d499d44a4686
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-14 20:22:17 +0300

    [ADD] Imports __init__ file

--- PyrapperTGStatAPI/__init__.py
+++ PyrapperTGStatAPI/__init__.py
@@ -4,7 +4,10 @@
 Copyright: (c) 2024 Evgeny
 """
 
-
+from .enums import *
+from .classes import *
+from .exceptions import *
+from .tg_stat import TGStatSync
 
 __author__ = "KrymmyEM"
 __version__ = "0.1.0"
