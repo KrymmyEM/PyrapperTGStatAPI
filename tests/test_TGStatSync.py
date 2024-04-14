952d460bf3c0d0a08df5a37ef5725a38558011c7
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-14 23:58:14 +0300

    [UP] Test

--- tests/test_TGStatSync.py
+++ tests/test_TGStatSync.py
@@ -2,7 +2,7 @@ from os import environ
 import unittest
 
 from dotenv import load_dotenv
-from PyrapperTGStatAPI import classes, enums, exceptions, tg_stat
+from krypper_tgstat import classes, enums, exceptions, tg_stat
 
 load_dotenv()
 
