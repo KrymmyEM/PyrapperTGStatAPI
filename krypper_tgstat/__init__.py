89e4738ea9ff3026453098681584c3af78f9e262
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-15 09:48:24 +0300

    [VER] 0.1.7

--- krypper_tgstat/__init__.py
+++ krypper_tgstat/__init__.py
@@ -10,4 +10,4 @@ from .exceptions import *
 from .tg_stat import TGStatSync
 
 __author__ = "KrymmyEM"
-__version__ = "0.1.6.3"
+__version__ = "0.1.7"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 Copyright: (c) 2024 Evgeny
 """
 
-version = "0.1.6.3"
+version = "0.1.7"
 
 long_description = ""
 
