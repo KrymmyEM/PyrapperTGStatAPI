93fbee231d0eead4d705f64a0d5028085fe05a56
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-04-18 16:24:55 +0300

    [ADD] kwargs parameters

--- krypper_tgstat/classes.py
+++ krypper_tgstat/classes.py
@@ -6,7 +6,7 @@ class Channel:
             self, id, link, username,
             title, about, image100, image640, participants_count, peer_type=None, 
             active_usernames=None, tgstat_restrictions = None, tg_id = None,
-            category = None, country = None, language = None
+            category = None, country = None, language = None, **kwargs
         ):
         self.id = id
         self.tg_id = tg_id
@@ -23,6 +23,8 @@ class Channel:
         self.category = None
         self.country = None
         self.language = None
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
 
@@ -33,7 +35,7 @@ class ChannelStatistic:
                  mentions_count=None, forwards_count=None, mentioning_channels_count=None, posts_count=None,
                  dau=None, wau=None, mau=None, online_count_day_time=None, online_count_night_time=None,
                  messages_count_yesterday=None, messages_count_last_week=None, messages_count_last_month=None,
-                 messages_count_total=None):
+                 messages_count_total=None, **kwargs):
         self.id = id
         self.title = title
         self.username = username
@@ -61,6 +63,7 @@ class ChannelStatistic:
         self.messages_count_last_week = messages_count_last_week
         self.messages_count_last_month = messages_count_last_month
         self.messages_count_total = messages_count_total
+        
 
 
 class Media:
@@ -74,7 +77,7 @@ class Story:
     def __init__(
         self, id, date, views, link,
         channel_id, is_expired, expire_at,
-        caption, media
+        caption, media, **kwargs
         ):
         self.id = id
         self.date = date
@@ -88,31 +91,39 @@ class Story:
         if media:
             self.media = Media(**media)
 
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 class User:
     def __init__(
         self, id, tg_id, username, 
-        firstname, lastname
+        firstname, lastname, **kwargs
         ):
         self.id = id
         self.tg_id = tg_id
         self.username = username
         self.firstname = firstname
         self.lastname = lastname
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
 class Mention:
-    def __init__(self, mentionId, mentionType, postId, postLink, postDate, channelId = None):
+    def __init__(self, mentionId, mentionType, postId, postLink, postDate, channelId = None, **kwargs):
         self.mentionId = mentionId
         self.mentionType = mentionType
         self.postId = postId
         self.postLink = postLink
         self.postDate = postDate
         self.channelId = channelId
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class Forward:
-    def __init__(self, postId, postLink, channelId, peerType=None, postDate=None, forwardId=0, sourcePostId=0):
+    def __init__(self, postId, postLink, channelId, peerType=None, postDate=None, forwardId=0, sourcePostId=0, **kwargs):
         if forwardId:
             self.forwardId = forwardId
         
@@ -124,6 +135,9 @@ class Forward:
         self.channelId = channelId
         self.peerType = peerType
         self.postDate = postDate
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class Post:
@@ -133,7 +147,7 @@ class Post:
             user_id = None, is_deleted = False, deleted_at="", 
             group_id=None, text="", snippet="", media = [], shares_count= None,
             comments_count = None, reactions_count = None
-        ):
+        , **kwargs):
         self.id = id
         self.date = date
         self.views = views
@@ -155,18 +169,24 @@ class Post:
 
         self.shares_count = shares_count
         self.comments_count = comments_count
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class MentionChannel:
-    def __init__(self, channel_id, mentions_count, views_count, last_mention_date):
+    def __init__(self, channel_id, mentions_count, views_count, last_mention_date, **kwargs):
         self.channel_id = channel_id
         self.mentions_count = mentions_count
         self.views_count = views_count
         self.last_mention_date = last_mention_date
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class UnionStatistic:
-    def __init__(self, viewsCount, sharesCount, reactionsCount, commentsCount=0, postId=0, storyId=0):
+    def __init__(self, viewsCount, sharesCount, reactionsCount, commentsCount=0, postId=0, storyId=0, **kwargs):
         if postId:
             self.postId = postId
         if storyId:
@@ -175,16 +195,22 @@ class UnionStatistic:
         self.sharesCount = sharesCount
         self.commentsCount = commentsCount
         self.reactionsCount = reactionsCount
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class View:
-    def __init__(self, date, viewsGrowth):
+    def __init__(self, date, viewsGrowth, **kwargs):
         self.date = date
         self.viewsGrowth = viewsGrowth
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class GroupStatistic:
-    def __init__(self, viewsCount=None, sharesCount=None, commentsCount=None, reactionsCount=None, forwardsCount=None, mentionsCount=None, forwards=[], mentions=[], views=[]):
+    def __init__(self, viewsCount=None, sharesCount=None, commentsCount=None, reactionsCount=None, forwardsCount=None, mentionsCount=None, forwards=[], mentions=[], views=[], **kwargs):
         self.viewsCount = viewsCount
         self.sharesCount = sharesCount
         self.commentsCount = commentsCount
@@ -197,6 +223,10 @@ class GroupStatistic:
             self.mentions = [Mention(**mention) for mention in mentions]
         if views:
             self.views = [View(**view) for view in views]
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class CallbackNotify:
@@ -215,14 +245,17 @@ class CallbackNotify:
 
 
 class DatabaseEntity:
-    def __init__(self, database_type: DatabaseTypes, code=None, name=None):
+    def __init__(self, database_type: DatabaseTypes, code=None, name=None, **kwargs):
         self.database_type = database_type
         self.code = code
         self.name = name
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class MassiveResult:
-    def __init__(self, result_type: ResultsType, count = None, total_count = None, channel = None, items = None, channels = None):
+    def __init__(self, result_type: ResultsType, count = None, total_count = None, channel = None, items = None, channels = None, **kwargs):
         self.result_type = result_type
         if count:
             self.count = count
@@ -252,6 +285,9 @@ class MassiveResult:
 
         if channels:
             self.channels = [Channel(**chan) for chan in channels]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 
 class DynamicData:
